f8277e8f735d2309e953fb21984474eabed50758
Shubham Telang <76528092+ShubhamTelang@users.noreply.github.com>
2021-03-15 18:35:38 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,47 @@
+import random
+x="y"
+while x=="y":
+    number=random.randint(1,6)
+
+    if number==1:
+        print("-----------")
+        print("|         |")
+        print("|    1    |")
+        print("|         |")
+        print("-----------")
+
+    if number==2:
+        print("-----------")
+        print("|         |")
+        print("|    2    |")
+        print("|         |")
+        print("-----------")
+
+    if number==3:
+        print("-----------")
+        print("|         |")
+        print("|    3    |")
+        print("|         |")
+        print("-----------")
+
+    if number==4:
+        print("-----------")
+        print("|         |")
+        print("|    4    |")
+        print("|         |")
+        print("-----------")
+
+    if number==5:
+        print("-----------")
+        print("|         |")
+        print("|    5    |")
+        print("|         |")
+        print("-----------")
+
+    if number==6:
+        print("-----------")
+        print("|         |")
+        print("|    6    |")
+        print("|         |")
+        print("-----------")
+    x=input("press y for new value ")
